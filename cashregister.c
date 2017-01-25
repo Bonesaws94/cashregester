0471cdab0e4de0a44dde4de01694c7d11f4e9ce0
Bonesaws94 <out.lander@live.ca>
2017-01-25 14:52:38 -0500

    Add files via upload

---
+++ cashregister.c
@@ -0,0 +1,31 @@
+// Bo Singh 028866135 sect i
+
+#include <stdio.h>
+
+int main() {
+
+	// Declare variables
+
+	float amount_to_be_paid;
+	float loonie;
+	float quarter;
+	float loonies_required;
+	float quarters_required;
+
+	loonie = 1.00;
+	quarter = 0.25;
+	amount_to_be_paid = 0.00;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
