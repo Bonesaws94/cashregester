232a8b6882e6dd0544305e47e56bc151fc7e39fb
Bonesaws94 <out.lander@live.ca>
2017-01-26 11:12:39 -0500

    Update cashregister.c

--- cashregister.c
+++ cashregister.c
@@ -11,10 +11,25 @@ int main() {
 	float quarter;
 	float loonies_required;
 	float quarters_required;
-
+        int workingamounta;    //this is the value for the ammount of loonies required before final calcaulation 
+	int workingamountb;   //this is the value for the ammount of quaters required before final calcaulation 
+	
 	loonie = 1.00;
 	quarter = 0.25;
 	amount_to_be_paid = 0.00;
+        //workingamounta = amount_to_be_paid * 100;
+	//workingamountb = amount_to_be_paid * 100;
+	
+	loonies_required = workingamounta % 100;
+	
+	printf("Hello. \n Please enter the amount to be paid \n.");
+	scanf("%f" , &amount_to_be_paid);
+	printf ("\n");
+	
+	
+
+	
+
 
 
 
