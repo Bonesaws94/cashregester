053de021f17d8d8b339bc6134fa7f5c892e033cc
Bonesaws94 <out.lander@live.ca>
2017-01-30 15:22:58 -0500

    Update cashregister.c

--- cashregister.c
+++ cashregister.c
@@ -14,23 +14,19 @@ int main() {
 	double dimes_owed;
 	double nickles_owed;
 	double pennies_owed;
-		
+	
+	float GST; // Value for TAX
+	
         int workingamounta;     //this is the value for the amount of loonies required before final calcaulation 
 	int workingamountb;    //this is the value for the amount of quaters required before final calcaulation 
 	int workingamountc;   //this is the value for the amount of dimes required before final calcaulation
 	int workingamountd;  //this is the value for the amount of nickles required before final calcaulation
 	int workingamountf;  //this is the value for the amount of pennys required before final calcaulation
-	loonie = 1.00;
-	quarter = 0.25;
-	amount_to_be_paid = 0.00;
-        //workingamounta = amount_to_be_paid * 100;
-	//workingamountb = amount_to_be_paid * 100;
 	
-	loonies_required = workingamounta % 100;
+	// Define variables
 	
-	printf("Hello. \n Please enter the amount to be paid \n.");
-	scanf("%f" , &amount_to_be_paid);
-	printf ("\n");
+	printf ("Please enter the amount you owe:$");
+	scanf ("
 	
 	
 
