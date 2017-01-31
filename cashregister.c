029e3892bc3790c63aef49ec4f299a9ef463116c
Bonesaws94 <out.lander@live.ca>
2017-01-31 10:40:08 -0500

    Update cashregister.c

--- cashregister.c
+++ cashregister.c
@@ -1,33 +1,50 @@
 // Bo Singh 028866135 sect i
 
 #include <stdio.h>
-
+#define GST 1.13 // Value for tax as a constant value
 int main() {
 
 	// Declare variables
 
 	double amount_to_be_paid;
-	double loonie;
-	double quarter;
-	double loonies_required;
-	double quarters_required;
+	double loonies_owed;
+	double quarters_owed;
 	double dimes_owed;
 	double nickles_owed;
 	double pennies_owed;
+	double balance;
 	
-	float GST; // Value for TAX
 	
-        int workingamounta;     //this is the value for the amount of loonies required before final calcaulation 
-	int workingamountb;    //this is the value for the amount of quaters required before final calcaulation 
-	int workingamountc;   //this is the value for the amount of dimes required before final calcaulation
-	int workingamountd;  //this is the value for the amount of nickles required before final calcaulation
-	int workingamountf;  //this is the value for the amount of pennys required before final calcaulation
+        int loonies, 
+	int quarters, 
+	int dimes, 
+	int nickels, 
+	int pennies;
 	
-	// Define variables
+	balance = amount_paid * GST;
 	
-	printf ("Please enter the amount you owe:$");
-	scanf ("
-	
+	printf("Please enter the amount to be paid: $");
+	scanf("%lf", &amount_to_be_paid
+	printf("GST: ", GST);
+	balance = amount__to_be_paid * GST;
+	printf("Balance owing: %.2lf", balance);
+	loonies = balance;
+	loonies_owed = balance - loonies;
+	quarters = loonies_owed / 0.25;
+	quarters_owed = loonies_owed - (quarters * 0.25);
+	dimes = quarters_owed / 0.10;
+	dimes_owed = quarters_owed - (dimes * 0.10);
+	nickels = dimes_owed / 0.05;
+	nickels_owed = dimes_owed - (nickels * 0.05);
+	pennies = nickels_owed / 0.01;
+	pennies_owed = nickels_owed - (pennies * 0.01);
+	printf("\nLoonies required: %d, balance owing $%.2lf", loonies, loonies_owed);
+	printf("\nQuarteres required: %d, balance owing $%.2lf", quarters, quarters_owed);
+	printf("\nDimes required: %d, balance owing $%.2lf", dimes, dimes_owed);
+	printf("\nNickels required: %d, balance owing $%.2lf", nickels, nickels_owed);
+	printf("\nPennies required: %d, balance owing $%.2lf\n", pennies, pennies_owed);
+return 0;
+}
 	
 
 	
